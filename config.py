5dfbdaff9e591c4954da0fc43b914b8058f0a9a9
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2024-04-25 09:23:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,9 +38,9 @@ OWNER_ID = int(getenv("OWNER_ID", "6367495275"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_APP_NAME = "pushakrmusjv" #getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_API_KEY = "HRKU-943f8dae-f1f3-429c-870d-1abd500e77fc" #getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
